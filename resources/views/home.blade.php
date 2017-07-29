8eccfb12003661c435265639bb1c6f05ae5e7766
Asus <avazalimov95@mail.ru>
2017-07-29 19:37:32 +0500

    Avaz committed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-
     <style>
         .navbar {
             background-color: #372e30;
