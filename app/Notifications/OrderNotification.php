b5516abf2e51f1ba8b5a57a31a8d1952e58d5e1d
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-08-19 12:45:45 +0500

    Validation fixed

---
+++ app/Notifications/OrderNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
+class OrderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     * @return array
+     * @internal param mixed $notifiable
+     */
+    public function via()
+    {
+        return [TelegramChannel::class];
+    }
+    public function toTelegram($order)
+    {
+        $point1 = explode(",", $order->point_A, 2);
+        $point2 = explode(",", $order->point_B, 2);
+        return TelegramMessage::create()
+            ->to(-237163806) // Optional.
+            ->content("*Грузоперевозка - Новый Заказ!*\n*Ид:*".$order->id."\n*Тариф:* ".$order->tarifName()."\n*Авто:* ".$order->getCarName()."\n*Имя:*".$order->name."\n*Тел:*".$order->phone."\n*Откуда:*".$order->address_A."\n*Куда:*".$order->address_B."\n*Время подачи:*".$order->start_time."\n*Цена:*".$order->sum
+             );
+
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     * @return MailMessage
+     * @internal param mixed $notifiable
+     */
+    public function toMail()
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     * @return array
+     * @internal param mixed $notifiable
+     */
+    public function toArray()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Notifications/TaxiOrderNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
+class TaxiOrderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [TelegramChannel::class];
+    }
+    public function toTelegram($order)
+    {
+        $point1 = explode(",", $order->point_A, 2);
+        $point2 = explode(",", $order->point_B, 2);
+        return TelegramMessage::create()
+            ->to(-237163806) // Optional.
+            ->content("*Такси - Новый Заказ!*\n*Ид:*".$order->id."\n*Дистанция:*".$order->distance."\n*Время ожидания (мин):*".$order->minute."\n*Авто:* ".$order->getCarName()."\n*Имя:*".$order->name."\n*Тел:*".$order->phone."\n*Откуда:*".$order->address_A."\n*Куда:*".$order->address_B."\n*Время подачи:*".$order->start_time."\n*Цена:*".$order->sum
+            );
+
+    }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
